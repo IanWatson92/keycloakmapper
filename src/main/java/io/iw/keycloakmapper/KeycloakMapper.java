f4458ab77e38458c2be4c14ad7504482bdd73722
Ian Watson <ianwatson@redhat.com>
2020-04-02 15:15:44 +0100

    updating inital run

---
+++ src/main/java/io/iw/keycloakmapper/KeycloakMapper.java
@@ -0,0 +1,43 @@
+package io.iw.keycloakmapper;
+
+import org.keycloak.models.KeycloakSession;
+import org.keycloak.models.ProtocolMapperModel;
+import org.keycloak.models.UserSessionModel;
+import org.keycloak.protocol.oidc.mappers.AbstractOIDCProtocolMapper;
+import org.keycloak.protocol.oidc.mappers.OIDCAccessTokenMapper;
+import org.keycloak.provider.ProviderConfigProperty;
+import org.keycloak.representations.IDToken;
+
+import java.util.List;
+
+public class KeycloakMapper extends AbstractOIDCProtocolMapper implements OIDCAccessTokenMapper {
+    @Override
+    public String getDisplayCategory() {
+        return "Token mapper";
+    }
+
+    @Override
+    public String getDisplayType() {
+        return "Project mapper";
+    }
+
+    @Override
+    public String getHelpText() {
+        return "Maps a project to the access token to be used for ABAC'd applications";
+    }
+
+    @Override
+    public List<ProviderConfigProperty> getConfigProperties() {
+        return null;
+    }
+
+    @Override
+    public String getId() {
+        return "project-mapper";
+    }
+
+    @Override
+    protected void setClaim(IDToken token, ProtocolMapperModel mappingModel, UserSessionModel userSession, KeycloakSession keycloakSession) {
+        token.getOtherClaims().put("test_claim", "Working!");
+    }
+}
